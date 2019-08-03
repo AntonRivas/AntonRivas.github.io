b9113ddc300ff90f0daaabcaaf738d2708071782
AntonRivas <antondearribas@gmail.com>
2019-08-03 15:55:12 +0100

    added contact page. burger and sidebar

--- static/script.js
+++ static/script.js
@@ -1,11 +1,25 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  document.querySelector('.top-container').onclick = () => {
-    window.location.href = "index.html";
+  //document.querySelector('.top-container').onclick = () => {
+    //window.location.href = "index.html";
+  //}
+
+//creating the sidebar
+  document.querySelector('.burger').onclick = () => {
+    console.log("nearly opened!");
+    document.querySelector("#mySidebar").style.width = "300px";
+    document.querySelector('.super').style.marginRight = "300px";
+    console.log("opened!");
   }
 
+  document.querySelector('.close-btn').onclick = () => {
+    document.querySelector("#mySidebar").style.width = "0";
+    document.querySelector(".super").style.marginRight= "0";
+        console.log("closed!");
+  }
 
+//creating the collapsible parts
   var coll = document.getElementsByClassName("collapsible");
   var i;
 
